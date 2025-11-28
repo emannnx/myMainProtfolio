849632313ad79544fa01fc547dc52993d5b620d4
Iman Olabode-Bello <imanolabodebello8@gmail.com>
2025-11-28 14:49:52 +0100

    new packages

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,7 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
+import Calender from "@/components/Calender";
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -15,6 +16,7 @@ const Index = () => {
       <About />
       <Projects />
       <Skills />
+      <Calender />
       <Contact />
       <Footer />
     </div>
